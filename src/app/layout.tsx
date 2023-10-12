d892d23c398a108e5f6c442806310634c14a82bb
alejandro <alejandrober96@gmail.com>
2023-10-12 17:23:21 -0500

    Change html language2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body >
         <Toaster />
         <Providers>
