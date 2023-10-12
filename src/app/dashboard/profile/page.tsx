5c958f0689ddf382045e70cd4967877ad697833d
alejandro <alejandrober96@gmail.com>
2023-10-12 17:24:32 -0500

    Add dispatch for calling the API and retrieve the invoices

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,9 +1,27 @@
 'use client'
 
 import { useSession } from "next-auth/react"
+import { useDispatch, useSelector } from "react-redux/es/exports";
+import { getInvoices } from "@/redux/features/invoiceSlice";
+import { useEffect } from "react";
+import {ThunkDispatch} from "@reduxjs/toolkit";
 
 function Dashboard() {
   const { data: session } = useSession();
+  const dispatch = useDispatch<ThunkDispatch<any, any, any>>();
+  const invoices = useSelector((store: any) => store.invoice.invoices);
+
+  
+  useEffect(() => {
+    if (session?.user) {
+      if(session?.user){
+        const userId = (session?.user as { _id: string })._id;
+        dispatch(getInvoices(userId));
+      }
+    }
+  }, [session, dispatch]);
+
+  console.log(invoices)
   return (
     <div>
       <h1>Perfil</h1>
@@ -13,10 +31,18 @@ function Dashboard() {
 
       <pre>
         {JSON.stringify(
-          {session,
-          status},
+          {session
+         },
            null, 2)}
       </pre>
+
+      {invoices.map((invoice: any) => (
+        <div key={invoice._id}>
+          <p>servicio: {invoice.service}</p>
+          <p>pago: {invoice.amount}</p>
+          <p>consumo: {invoice.consumption}</p>
+        </div>
+      ))}
     </div>
   )
 }
