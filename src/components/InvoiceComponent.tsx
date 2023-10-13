69562ec4196d12da719e2010988bee70e5dea568
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-10-13 14:42:29 -0500

    Add delete invoice v1

--- src/components/InvoiceComponent.tsx
+++ src/components/InvoiceComponent.tsx
@@ -1,13 +1,17 @@
 import { invoiceType } from "@/redux/features/invoiceSlice"
 
-function InvoiceComponent({date, service, amount, consumption}: invoiceType) {
+function InvoiceComponent({date, service, amount, consumption, _id}: invoiceType) {
+  const handleClick = () => {
+    console.log(_id)
+  }
   return (
     <div >
-          <p>servicio: {service}</p>
-          <p>pago: {amount}</p>
-          <p>consumo: {consumption}</p>
-          <p>fecha: {date.toString()}</p>
-        </div>
+      <p>servicio: {service}</p>
+      <p>pago: {amount}</p>
+      <p>consumo: {consumption}</p>
+      <p>fecha: {date.toString()}</p>
+      <button onClick={handleClick}>Eliminar</button>
+    </div>
   )
 }
 
