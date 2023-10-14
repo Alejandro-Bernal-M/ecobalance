ccab0668732daf3d0846237e7326b592cd1443e7
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-10-14 09:50:36 -0500

    Add asyncthunk for getting the alerts

--- src/redux/features/alertSlice.ts
+++ src/redux/features/alertSlice.ts
@@ -1,13 +1,12 @@
-import { createSlice } from "@reduxjs/toolkit";
-// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-// import axios, {AxiosError} from 'axios';
-// import { Date } from 'mongoose';
-// import toast from 'react-hot-toast';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios, {AxiosError} from 'axios';
+import toast from 'react-hot-toast';
 
-type alertType = {
+export type alertType = {
   amount: number,
   user_id: string,
-  service: 'water' | 'gas' | 'electricity'
+  service: 'water' | 'gas' | 'electricity',
+  _id: string
 }
 
 type alertInitialStateType = {
@@ -22,10 +21,41 @@ const initialState = {
   error: ''
 } as alertInitialStateType
 
+export const getAlerts = createAsyncThunk(
+  'alerts/getAlerts',
+  async (user_id: string) =>{
+    try {
+      const response = await axios.get(`/api/alerts?user_id=${user_id}`);
+      if(response.status == 200){
+        return response.data
+      }
+    } catch (error) {
+      if(error instanceof AxiosError){
+        console.log(error);
+        toast.error('Ha ocurrido un error');
+        return;
+      }
+    }
+  }
+)
+
 const alertSlice = createSlice({
   name: 'alert',
   initialState,
-  reducers: {}
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getAlerts.pending, (state) => {
+      state.loading = true;
+    })
+    builder.addCase(getAlerts.fulfilled, (state, action) => {
+      state.alerts = action.payload.alerts;
+      state.loading = false;
+    })
+    builder.addCase(getAlerts.rejected, (state) => {
+      state.loading = false;
+      state.error = 'Error fetching alerts';
+    })
+  }
 })
 
 export default alertSlice.reducer
