2eb463779eb6276b2f72aefdae7e5f94cebff684
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-10-14 09:21:45 -0500

    Add API endpoints for getting all the alerts and posting new alerts

---
+++ src/app/api/alerts/route.ts
@@ -0,0 +1,42 @@
+import { connectDB } from "@/libs/mongodb";
+import Alert from "@/models/alert";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request){
+  const url = new URL(request.url);
+  const user_id = url.searchParams.get('user_id');
+
+  try {
+    await connectDB();
+
+    const alerts = await Alert.find({user: user_id})
+
+    return NextResponse.json({alerts});
+  } catch (error) {
+    if(error instanceof Error){
+      console.log(error);
+      return NextResponse.json({error: error.message}, {status: 500})
+    }
+  }
+};
+
+export async function POST(request: Request){
+  const { amount, service, user_id } = await request.json()
+
+  try {
+    await connectDB();
+    const alert = new Alert({
+      amount,
+      service,
+      user: user_id
+    })
+    const savedAlert = await alert.save();
+
+    return NextResponse.json({savedAlert})
+  } catch (error) {
+    if(error instanceof Error){
+      console.log(error);
+      return NextResponse.json({message: error.message}, {status: 500})
+    }
+  }
+}
