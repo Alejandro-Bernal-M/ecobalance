fbfc7a11d65c5c8eccadc723ee34fc382c8ab02d
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-10-14 06:26:35 -0500

    Add delete endpoint

--- src/app/api/invoices/route.ts
+++ src/app/api/invoices/route.ts
@@ -29,7 +29,6 @@ export async function POST(request: Request){
     try {
       
     await connectDB();
-    console.log('post')
     const invoice = new Invoice({
       user: user_id,
       amount,
@@ -51,3 +50,24 @@ export async function POST(request: Request){
     }
 }
 
+export async function DELETE(request: Request){
+  const url = new URL(request.url);
+  const invoice_id = url.searchParams.get('invoice_id');
+
+  try {
+    await connectDB();
+
+    const deletedInvoice = await Invoice.findByIdAndDelete(invoice_id);
+
+    if (deletedInvoice) {
+      return NextResponse.json({ message: 'Invoice deleted successfully', deletedInvoice });
+    } else {
+      return NextResponse.json({ message: 'Invoice not found', status: 404 });
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error);
+      return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+  }
+}
