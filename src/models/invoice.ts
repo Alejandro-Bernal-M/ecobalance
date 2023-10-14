9b4e2099bf4df605c9e99f9e0964b40ecbbd3929
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-10-14 07:14:21 -0500

    update invoice model to improve security

--- src/models/invoice.ts
+++ src/models/invoice.ts
@@ -1,16 +1,27 @@
 import { Schema, model, models } from "mongoose";
 
 const InvoiceSchema = new Schema({
-  date: Date,
-  consumption: Number,
+  date:{
+    type: Date,
+    required: [true, 'la fecha es requerida']
+  },
+  consumption:{
+    type: Number,
+    required: [true, 'El consumo es requerido']
+  },
   service: {
     type: String,
     enum: ['water', 'gas', 'electricity'],
+    required: [true, 'El servicio es requerido']
   },
-  amount: Number,
+  amount: {
+    type: Number,
+    required: [true, 'La cantidad es requerida']
+  },
   user: {
     type: Schema.Types.ObjectId,
     ref: 'User',
+    required: true
   }
 })
 
