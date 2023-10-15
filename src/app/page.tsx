e6debed9c85b34c45ac35d6273b403c91e641404
alejandro <alejandrober96@gmail.com>
2023-10-14 19:17:31 -0500

    Add styles to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import styles from './page.module.css';
+import { ImLeaf } from 'react-icons/im';
+
 export default function Home() {
   return (
-    <div>
-      hello world
-    </div>
+    <section  className={styles.section}>
+      <div className={styles.main_div}>
+        <h1>EcoBalance <ImLeaf /></h1>
+        <p>Sigue el consumo de tu hogar.</p>
+      </div>
+    </section>
   )
 }
