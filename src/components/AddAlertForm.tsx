8796195678e6e2e7db4244d008e545a2419a061c
alejandro <alejandrober96@gmail.com>
2023-10-16 08:34:20 -0500

    Add Alert form styles

--- src/components/AddAlertForm.tsx
+++ src/components/AddAlertForm.tsx
@@ -3,6 +3,7 @@ import toast from "react-hot-toast";
 import { addAlert } from "@/redux/features/alertSlice";
 import { useDispatch } from "react-redux/es/exports";
 import {ThunkDispatch} from "@reduxjs/toolkit";
+import styles from './AddAlertForm.module.css'
 
 function AddAlertForm({user}: any) {
   const dispatch = useDispatch<ThunkDispatch<any, any, any>>();
@@ -28,20 +29,27 @@ function AddAlertForm({user}: any) {
     
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="service-select"> Seleccione el servicio </label>
-      <select name="service-select" defaultValue={''}>
-        <option value="" ></option>
-        <option value="water" id="water">Agua</option>
-        <option value="gas" id="gas">Gas</option>
-        <option value="electricity" id="electricity">Electricidad</option>
-      </select>
-      <label htmlFor="amount"> Valor de la alerta </label>
-      <input type="number" name="amount" placeholder="" />
-      <button>
+    <>
+    <h2 className={styles.title}>Agregar Alerta</h2>
+    <form className={styles.form} onSubmit={handleSubmit} >
+      <div className={styles.form_div}>
+        <label htmlFor="service-select"> Seleccione el servicio </label>
+        <select className={styles.form_select} name="service-select" defaultValue={''}>
+          <option value="" ></option>
+          <option value="water" id="water">Agua</option>
+          <option value="gas" id="gas">Gas</option>
+          <option value="electricity" id="electricity">Electricidad</option>
+        </select>
+      </div>
+      <div className={styles.form_div}>
+        <label htmlFor="amount"> Valor de la alerta </label>
+        <input className={styles.form_input} type="number" name="amount" placeholder="" />
+      </div>
+      <button className={styles.form_button}>
         Añadir
       </button>
     </form>
+    </>
   )
 }
 
