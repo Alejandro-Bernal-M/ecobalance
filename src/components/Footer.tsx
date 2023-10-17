e91b4e88c41d589019eee7e1415746c3bd21a449
alejandro <alejandrober96@gmail.com>
2023-10-16 20:39:57 -0500

    Update footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,11 @@ import styles from './footer.module.css';
 
 function Footer() {
   return (
-    <footer className={styles.footer}>Alejandro Bernal 2023</footer>
+    <footer className={styles.footer}>
+      <p>Realizado por Valeria Zamudio, Carolina Mesa y Juan David Arteaga </p>
+      <p>Materia innovadores Nuestra Señora de Fátima</p>
+      <p> EcoBalance 2023</p>
+    </footer>
   )
 }
 
